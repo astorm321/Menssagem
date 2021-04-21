c763ca98668ff8d46cbc1c603d171819608396f5
Vitor Pereira <vitor123@live.com.pt>
2021-04-21 12:18:07 +0100

    Mostrar a menssagem enviada

--- app/src/main/java/com/example/menssagem/MostraMenssagemActivity.kt
+++ app/src/main/java/com/example/menssagem/MostraMenssagemActivity.kt
@@ -2,10 +2,16 @@ package com.example.menssagem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MostraMenssagemActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mostra_menssagem)
+
+        val menssagem = intent.getStringExtra("MENSSAGEM")
+
+        val textViewMenssagem = findViewById<TextView>(R.id.textViewMenssagem)
+        textViewMenssagem.text = menssagem
     }
 }
