2b492f9a29eb5200617ad74e66f521250b0145cf
Vitor Pereira <vitor123@live.com.pt>
2021-04-21 11:31:27 +0100

    Adicionar actividade para mostrar menssagem

---
+++ app/src/main/java/com/example/menssagem/MostraMenssagemActivity.kt
@@ -0,0 +1,11 @@
+package com.example.menssagem
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MostraMenssagemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mostra_menssagem)
+    }
+}
